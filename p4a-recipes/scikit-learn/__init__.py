f0758b697142927d65236b7a5dfb3b3172b071e3
Karl Semich <0xloem@gmail.com>
2020-11-09 14:03:26 -0500

    recipe fixes

--- p4a-recipes/scikit-learn/__init__.py
+++ p4a-recipes/scikit-learn/__init__.py
@@ -1,12 +1,11 @@
-from pythonforandroid.toolchain import CythonRecipe, shprint, current_directory
-from os.path import exists, join
-import sh
-import glob
+from pythonforandroid.recipe import CythonRecipe
 
 
 class SKLearnRecipe(CythonRecipe):
     version = '0.23.2'
     url = 'https://github.com/scikit-learn/scikit-learn/archive/{version}.tar.gz'
+    md5sum = '8b4052a71ac81c0032e6d9ff693c6a0f'
+    sha512sum = 'e9797185b1b9def3ee5525e9b72742784aeb78fe8b069bb5aeab1d0f4513cf738c4489ed894274606dea9d6a32ccd3df26170ce1951e443dc77f9287c372e8c5'
 
     depends = ['python3', 'numpy', 'scipy', 'joblib', 'threadpoolctl']
 
