a10a95dd9066bb079f1529107fac34c2991c67a9
Karl Semich <0xloem@gmail.com>
2020-11-09 07:20:01 -0500

    -

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+# I made this app around 2014, based on a newspaper article I read.
+# All it did was show the heartpulse of the person in the image.
+# As I planned extending it to draw peoples' bloodflow in realtime, my life fell into shambles.
+# I expect it could be similarly extended to isolate every muscle motion.
+# The goal was simply to make a tool where people could use ICA to look at realtime data in their surroundings.
+#   Hence, a reasonable advancement would be factoring out the camera into the user adding data sources.
+
 from sklearn.decomposition import FastICA
 import numpy as np
 import sys
@@ -17,6 +24,8 @@ from kivy.uix.togglebutton import ToggleButton
 
 def tex2darray(texture):
         return np.frombuffer(texture.pixels, dtype=np.uint8).reshape(texture.height, texture.width * 4)
+def tex3darray(texture):
+        return np.frombuffer(texture.pixels, dtype=np.uint8).reshape(texture.height, texture.width, 4)
 
 class Graph(Image):
     def __init__(self, colors=(Color(1,0,1)), *args, **kwargs):
@@ -73,9 +82,15 @@ class TestApp(App):
         return parent
     def on_gobutton(self, gobutton):
         if self.index not in self.cameras:
-            self.cameras[self.index] = Camera(index = self.index, play = True)
-            self.cameras[self.index]._camera.bind(on_texture = self.on_camtexture)
+            camera = Camera(index = self.index, play = True)
+            camera._camera.bind(on_texture = self.on_camtexture)
+            camera._camera.widget = camera
+            self.cameras[self.index] = camera
             self.parent.add_widget(self.cameras[self.index], index=1)
+            camera.texrect = np.array([[-1/3,-1/3],[2/3,2/3]])
+            with camera.canvas.after:
+                Color(1,1,0)
+                camera.box = Line()
         elif not self.cameras[self.index].play:
             self.parent.add_widget(self.cameras[self.index], index=1)
             self.cameras[self.index].play = not self.cameras[self.index].play
@@ -91,12 +106,34 @@ class TestApp(App):
         if play:
             self.on_gobutton(self.gobutton)
     def on_camtexture(self, camera):
-        center = np.array(camera.texture.size)//2
-        image = tex2darray(camera.texture)
-        values = []
-        for x in range(2):
-            for y in range(2):
-                values.append(image[center[1] + y, center[0] + x] / 127.5 - 1)
+        texdims = np.array(camera.texture.size)
+        imgdims = np.array(camera.widget.size)
+        # when transforming x and y are different
+        # basically it's centered, and the smallest scale is made
+        # so we don't multiple by camera.size, we multiple by min
+        imgcenter = imgdims / 2
+        scale = np.min(imgcenter)
+        rect = np.array(camera.widget.texrect)
+        rect *= scale
+        rect[0] += imgcenter
+        camera.widget.box.rectangle = tuple(rect.flatten())
+        texcenter = texdims / 2
+        texscale = texdims / 2
+
+        image = tex3darray(camera.texture)
+        # interest region is within the rectangle and contains only the rgb channels
+        #print('hrm', camera.widget.texrect, texscale, texcenter)
+        texrect = np.array(camera.widget.texrect) * texscale
+        texrect[1] += texcenter
+        texrect[0] += texrect[1]
+        #print(texrect)
+        texrect = texrect.astype(np.uint)
+        #print(texrect)
+        interest = image[texrect[0][1]:texrect[1][1],texrect[0][0]:texrect[1][0],:3]
+        #print(interest)
+
+        # mean rgb along X and Y axes of all pixels in interest
+        values = interest.mean((0,1)) / 255
         self.graph.add(*values)
 
 
